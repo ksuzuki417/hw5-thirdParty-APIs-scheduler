fa974ab16b3ed592ca56d6b6dda0d7b2679467b1
Keita Suzuki <ksuzuki417@gmail.com>
2020-08-21 20:08:44 -0500

    change color function added but incomplete

--- javascript.js
+++ javascript.js
@@ -5,20 +5,24 @@ $(document).ready(function(){
     console.log(today)
     //displaying date
     $(currentDay.append(today))
-    //Global variables
-
+    
+    //create time blocks
     var timeBlocks = ["8am", "9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm", "6pm"];
-
+    var militaryHour = ["8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18"];
     for (var i = 0; i < timeBlocks.length; i++) {
+        //create variables in each time block.
         var scheduler = $("<div>");
         var timeDiv = $("<div>");
         var notesDiv = $("<textarea>");
         var buttonEl = $("<button>");
         var iconEl = $("<i>");
 
-        scheduler.attr("data-hour", timeBlocks[i])
+        scheduler.attr("data-hour", timeBlocks[i]);
+        scheduler.attr("id", militaryHour[i]);
+        //adding class attribute as row.
         scheduler.attr("class", "row");
-
+        timeDiv.attr("class", "hour col-1");
+        notesDiv.attr("class", "schedule col-9");
         buttonEl.attr("class", "saveBtn")
 
         $(".container").append(scheduler)
@@ -28,23 +32,24 @@ $(document).ready(function(){
 
         timeDiv.text(timeBlocks[i])
     }
+    var currentTime = moment().hour();
+    //color code based on time (past, present, future)
+    if(militaryHour[i] == currentTime) {
+        notesDiv.addClass("present");
+    }
+    else if (militaryHour[i] > currentTime) {
+        notesDiv.addClass("future");
+    } else{
+        notesDiv.addClass("past");
+    }
+    console.log(moment().hour())
 })
 
+//place text "SAVE" vertially on save button.
 
-//display current day on top
-//create time blocks for standard business hours
-
-
-//var timeBlock8 = $("<div>").text("8AM");
-//console.log(eight)
-
-//$(".container").append(timeBlock8);
-
-
-
-//color code based on time (past, present, future)
 //click on timeblock 
 //enter an event
 //create save button for that time block
 //save the text in local storage
+
 //saved events persist after refresh
